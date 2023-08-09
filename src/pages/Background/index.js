74e005644503c8843f2364d5371c8560c3614455
James Akpan <jamesakpan016@gmail.com>
2023-08-09 19:21:54 +0100

    Deleting useless files

--- src/pages/Background/index.js
+++
@@ -1,8 +0,0 @@
-const keepAlive = () =>
-  setInterval(() => {
-    chrome.runtime.getPlatformInfo();
-  }, 20e3);
-chrome.runtime.onStartup.addListener(keepAlive);
-
-//You can delete this if you don't need to keep your extension running in the background
-keepAlive();
