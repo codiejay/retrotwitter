db753c4fa4b0400abb785e9dbb9269fb320f01af
James Akpan <jamesakpan016@gmail.com>
2023-08-09 21:13:35 +0100

    Updating logo and post text

---
+++ scripts/content.js
@@ -0,0 +1,42 @@
+const replaceSvgWithImage = () => {
+  const twitterLogo = document.querySelector('[aria-label="Twitter"]');
+  const svgElement = twitterLogo?.querySelector('svg');
+
+  if (!svgElement) return;
+
+  const imgElement = document.createElement('img');
+  imgElement.style.width = '30px';
+  imgElement.src =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553';
+  svgElement.replaceWith(imgElement);
+};
+
+const updatePostSpanText = () => {
+  const composeTweet = document.querySelector('[href="/compose/tweet"]');
+  const spanElements = composeTweet?.querySelectorAll('span');
+
+  if (!spanElements) return;
+
+  for (let span of spanElements) {
+    if (span.textContent.trim() === 'Post') {
+      span.innerText = 'Tweet';
+      break;
+    }
+  }
+};
+
+const updateSiteIcon = () => {
+  //Update the site icon to twitter icon
+  const siteIcon = document.querySelector('link[rel="shortcut icon"]');
+  siteIcon.href =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553';
+    
+};
+
+window.onload = () => {
+  setTimeout(() => {
+    replaceSvgWithImage();
+    updatePostSpanText();
+    updateSiteIcon();
+  }, 500);
+};
